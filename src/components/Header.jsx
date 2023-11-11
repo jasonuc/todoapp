e3a9c79679e31013ee4bc43d16991f99ea5f5e68
jasonuc <mrjasonu@gmail.com>
2023-11-11 23:38:58 +0000

    Update Header component height

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 
 function Header() {
     return (
-      <div className="w-screen h-20 border-b-2 border-solid flex items-center justify-center text-3xl md:text-5xl font-bold">
+      <div className="w-screen h-14 md:h-16 border-b-2 border-solid flex items-center justify-center text-3xl md:text-5xl font-bold">
           <h1 className="text-white">todo</h1>
       </div>
     )
