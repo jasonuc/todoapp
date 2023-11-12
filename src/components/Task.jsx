6ce23f031362a0c41c00d5fdc76f41f549f6a3d9
jasonuc <mrjasonu@gmail.com>
2023-11-12 11:33:09 +0000

    improves design

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -8,7 +8,10 @@ function Task({ id, task, datetime }) {
 
     return (
         <div className="w-[100%] min-h-[3rem] border-2 border-solid rounded-md flex ">
-            <p className=' w-[90%] md:w-[95%] min-h-full p-2 text-white flex flex-wrap overflow-ellipsis items-center font-bold border-r-2 md:border-none rounded-tr-md rounded-br-md'>{`${task} | ${datetime}`}</p>
+            <div className='flex flex-col w-[90%] md:w-[95%] p-2  border-r-2 md:border-none rounded-tr-md rounded-br-md'>
+            <p className=' max-h-full text-white flex items-center font-bold'>{`${task}`}</p>
+            <p className=' text-xs text-third font-semibold italic underline'>{`Added: ${datetime}`}</p>
+            </div>
             <button onClick={() => removeTask(id)} className='text-white w-[10%] md:w-[5%] min-h-[100%] bg-green-400 flex-grow flex items-center md:border-l-2 md:rounded-tl-md md:rounded-bl-md justify-center rounded-tr-md rounded-br-md'><MdDoneOutline size={20} /></button>
         </div>
     )
