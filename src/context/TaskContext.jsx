57396dcfbe300999aded9e5cccc6a11e8dc2e9fd
jasonuc <mrjasonu@gmail.com>
2023-11-12 12:51:01 +0000

    adds google auth and storage

--- src/context/TaskContext.jsx
+++ src/context/TaskContext.jsx
@@ -1,29 +1,71 @@
-import { createContext, useEffect, useState } from 'react'
-import { db } from '../firebase/firebase'
+import { createContext, useEffect, useState } from 'react';
+import { db, auth } from '../firebase/firebase';
 
-export const TaskContext = createContext(null)
+export const TaskContext = createContext(null);
 
 export function TaskContextProvider({ children }) {
-  
-  const [tasks, setTasks] = useState(localStorage.getItem('tasks') ? JSON.parse(localStorage.getItem('tasks')) : [])
+  const [tasks, setTasks] = useState([]);
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
 
   useEffect(() => {
-    localStorage.setItem('tasks', JSON.stringify(tasks))
-  }, [tasks])
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      // If the user is logged in, user will be truthy; otherwise, it will be falsy.
+      setIsAuthenticated(!!user);
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        if (isAuthenticated) {
+          const snapshot = await db.collection('tasks').get();
+          const data = snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+          setTasks(data);
+        } else {
+          const localTasks = localStorage.getItem('tasks');
+          if (localTasks) {
+            setTasks(JSON.parse(localTasks));
+          }
+        }
+      } catch (error) {
+        console.error('Error fetching tasks:', error);
+      }
+    };
+
+    fetchData();
+  }, [isAuthenticated]);
+
+  useEffect(() => {
+    const updateData = async () => {
+      try {
+        if (isAuthenticated) {
+          await db.collection('tasks').set(tasks);
+        } else {
+          localStorage.setItem('tasks', JSON.stringify(tasks));
+        }
+      } catch (error) {
+        console.error('Error updating tasks:', error);
+      }
+    };
+
+    updateData();
+  }, [isAuthenticated, tasks]);
 
   const removeTask = (id) => {
     setTasks(prevTasks => prevTasks.filter(task => task.id !== id));
-  }
+  };
 
   const values = {
     tasks,
     setTasks,
     removeTask
-  }
+  };
 
   return (
     <TaskContext.Provider value={values}>
       {children}
     </TaskContext.Provider>
-  )
+  );
 }
