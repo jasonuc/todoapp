e8402204e466c3ee0982ac794047ad90a5c7440e
jasonuc <mrjasonu@gmail.com>
2023-11-12 10:51:08 +0000

    attempt at fixing 404 error

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,23 @@
-import { Route, Routes } from "react-router-dom"
-import Header from "./components/Header"
-import Login from "./pages/Login"
-import Home from "./pages/Home"
-import SignOutAndUsername from "./components/SignOutAndUsername"
-import NotFound from "./components/NotFound"
+import { Route, Routes } from "react-router-dom";
+import Header from "./components/Header";
+import Login from "./pages/Login";
+import Home from "./pages/Home";
+import SignOutAndUsername from "./components/SignOutAndUsername";
+import NotFound from "./components/NotFound";
 
 function App() {
   return (
-    <div className=" bg-primary min-h-screen w-screen font-sometypeMono flex flex-col items-center gap-y-2 text-navtext">
+    <div className="bg-primary min-h-screen w-screen font-sometypeMono flex flex-col items-center gap-y-2 text-navtext">
       <Header />
-
-      <Routes>
-        <Route path="/home" element={<SignOutAndUsername />} />
-      </Routes>
 
       <Routes>
         <Route index element={<Login />} />
         <Route path="/home" element={<Home />} />
+        <Route path="/home" element={<SignOutAndUsername />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
